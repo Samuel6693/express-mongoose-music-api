b60b25f4a25cded3ea348012b099e07f7e33c97c
Samuel6693 <samuel_somy93@hotmail.com>
2026-03-17 17:56:24 +0100

    Add comments to artist.js

--- db/artists.js
+++ db/artists.js
@@ -1,20 +1,24 @@
 import {Artist} from '../models/Artist.js';
 
+// Get all artists
 export async function getAllArtists() {
     return await Artist.find();
 }
 
+// Get artist by Id
 export async function getArtistById(id) {
     const artist = await Artist.findById(id);
     return artist ? artist : null;
 }
 
+// Create new artist
 export async function createArtist(name) {
     const artist = new Artist({name});
     await artist.save();
     return artist;
 }
 
+// Update artist by Id
 export async function updateArtist(id, name) {
     const artist = await Artist.findById(id);
     if (!artist) {
@@ -26,6 +30,7 @@ export async function updateArtist(id, name) {
     return artist;
 }
 
+// Delete artist by Id
 export async function deleteArtist(id) {
     const result = await Artist.deleteOne({_id: id}); // Deletes the artist with the specified ID
     return result.deletedCount === 1; // Returns true if an artist was deleted, false otherwise

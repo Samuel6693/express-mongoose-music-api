01d0a469b1dc5c83232951dfae383f66183c4167
Samuel Soumi <samuel_somy93@hotmail.com>
2026-03-24 15:03:27 +0100

    Improve error handling in the API by adding specific response for CastError

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ app.use("/api/albums", albumRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ error: "Internal server error" });
+
+  if (err.name === "CastError") {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+
+  res.status(500).json({
+    error: "Internal server error",
+    details: err.message,
+  });
 });
 
 async function startServer() {

dbf92dcd368779b674162329b32aef76186d7f92
Samuel Soumi <samuel_somy93@hotmail.com>
2026-03-25 14:04:33 +0100

    Add access token generation and improve login error handling

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import {express} from 'express';
 import { User } from '..models/User.js'
+import { generateAccessToken } from '../utils/token';
 
 
 const userRouter = express.Router(); 
@@ -50,18 +51,25 @@ userRouter.post('/login', async (req, res)=> {
         const existingUser = await User.findOne({ email }).select("+password");
 
         if (!existingUser) {
-            return res.status(404).json({ message: "User credential are not valid"})
+            return res.status(401).json({ message: "User credential are not valid"})
         }
 
-        const isMatch = await existingUser.comparepassword(password);
+        const isMatch = await existingUser.comparePassword(password);
 
         if (!isMatch) {
             return res.status(401).json({ message: "User credential are not valid"})
         }
-        res.status(200).json({ message: "Login successful", user: {
-            id: existingUser._id,
-            username: existingUser.username
-        }});
+
+        const accessToken = generateAccessToken(existingUser._id);
+
+        res.status(200).json({ 
+            message: "Login successful", 
+            user: {
+                id: existingUser._id,
+                username: existingUser.username
+            }, 
+            accessToken
+        });
     } catch (error) {
         res.status(500).json({ message: "Server error"});
     }

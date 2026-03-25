eef4c15ef6dd802b56bf359940c717c48608a266
Samuel Soumi <samuel_somy93@hotmail.com>
2026-03-25 10:17:52 +0100

    Add User model with schema, password hashing, and comparison method

---
+++ models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userScehma = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 3
+        }, 
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 8,
+            select: false
+        }
+    }, { timestamps: true}
+);
+
+// Add method to comapre passwords
+userScehma.methods.comparepassword = async function (plainpassword) {
+    return await bcrypt.compare(plainpassword, this.password);
+}
+
+// Middleware
+userScehma.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+
+    next();
+});
+
+
+export default User = mongoose.model("User", userScehma);
